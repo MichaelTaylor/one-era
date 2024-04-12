490bd8b342c4c3dfc344223c0362eb97cc23f711
Michael Taylor <michael.taylor.gamedev@gmail.com>
2024-04-12 03:01:42 -0500

    Changed back to author comments

--- frontend/src/components/Pages/Chapter Display/ChapterDisplay.js
+++ frontend/src/components/Pages/Chapter Display/ChapterDisplay.js
@@ -30,7 +30,7 @@ const ChapterDisplay = (props) => {
   const { windowOpen } = useOpenLink(link);
 
   const buttonStyle = `bg-primary mx-auto w-44 md:w-56 h-20 text-2xl text-white transition duration-300 font-bold 
-    hover:opacity-60 py-2 px-4 rounded-full mx-12 sm:h-14 sm:text-md lg:mx-100 my-2
+    hover:opacity-60 py-2 px-4 rounded-full mx-12 sm:h-20 sm:text-md md:text-lg lg:mx-100 my-2
     disabled:opacity-50`;
 
   const volumeButton = (
@@ -49,7 +49,7 @@ const ChapterDisplay = (props) => {
       disabled={mediaData.chapterData === ""}
       onClick={() => setAuthorComments(!authorComments)}
     >
-      {!authorComments ? "Comments" : "Cover"}
+      {!authorComments ? "Author Comments" : "Cover"}
     </button>
   );
 
